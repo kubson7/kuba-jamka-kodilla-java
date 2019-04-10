22f524ded4b60fb1c02d908be9163c0dd938f9a1
kubson10 <kubson9404@gmail.com>
2019-04-10 21:16:27 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error !");
+        }
+        // test Calculator
+        Calculator calculate = new Calculator();
+
+        System.out.println(calculate.Add(7,8));
+        System.out.println(calculate.Subtrack(7,8));
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a ;
+    private int b;
+
+    public int Add (int a,int b){
+        return a + b;
+    }
+    public int Subtrack (int a,int b){
+        return a - b;
+    }
+}
