8bdb9e09e6f82dbe692e538c7b0c4eb93d6785e4
kubson10 <kubson9404@gmail.com>
2019-05-30 11:31:15 +0200

    Commit message

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,13 +1,4 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    private int a ;
-    private int b;
-
-    public int add (int a,int b){
-        return a + b;
-    }
-    public int subtract (int a,int b){
-        return a - b;
-    }
 }
