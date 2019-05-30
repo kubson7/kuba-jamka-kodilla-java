2e2f5309198c6044c81a656e8286e35c2059076d
kubson10 <kubson9404@gmail.com>
2019-05-30 11:47:07 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,4 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+    public static void main (String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        // test pierwszy jest jednostkowy
+        System.out.println("Test - pierwszy test jednostkowy");
+        System.out.println("Test - drugi test jednostkowy prostego kalkulatora");
+
+        Calculator calculator = new Calculator();
+        int resume = calculator.add(44,11);
+        System.out.println(resume);
+        int resume2 = calculator.subtract(44,11);
+        System.out.println(resume2);
+    }
 }
