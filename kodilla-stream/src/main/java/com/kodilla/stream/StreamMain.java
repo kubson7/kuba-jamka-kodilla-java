6b6dfb122154e812228a33f99056eb05badaf0f5
kubson10 <kubson9404@gmail.com>
2019-06-22 16:03:18 +0200

    Immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
@@ -33,6 +34,9 @@ public class StreamMain {
         poemBeautifier.beautify("Ala ma kota",(text -> text.toUpperCase()));
         poemBeautifier.beautify("Ala MA kOTa",(text -> text.toLowerCase()));
         poemBeautifier.beautify("Ala ma kota",(text -> "ABC "+text));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(40);
     }
 
 }
