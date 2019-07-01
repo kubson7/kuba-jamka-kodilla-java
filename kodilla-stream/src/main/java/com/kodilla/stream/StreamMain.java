9ba1940159954667c87d24cb776d786c38fc9b69
kubson10 <kubson9404@gmail.com>
2019-07-01 21:38:58 +0200

    Moduł7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,22 +7,22 @@ import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.forumuser.Sex;
 
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-
         Forum forum = new Forum();
-        Map<Integer, ForumUser> selectedForumUser = forum.getUserList().stream()
-                .filter(sex -> sex.getSex() == Sex.M)
-                .filter(date -> date.getBirthday().getYear() <= 1999)
-                .filter(posts -> posts.getPublishedPosts()>0)
-                .collect(Collectors.toMap(ForumUser::getID, forumUser -> forumUser));
+        Map<Integer, ForumUser> theResultMapOfUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex().equals(Sex.M))
+                .filter(forumUser -> forumUser.getBirthday().getYear()<1999)
+                .filter(forumUser -> forumUser.getPublishedPosts()>1)
+                .collect(Collectors.toMap(ForumUser::getID, ForumUser ->ForumUser));
 
-        System.out.println("# elements: " + selectedForumUser.size());
-            selectedForumUser.entrySet().stream()
-                    .map(entry -> entry.getKey()+ " ==> "+ entry.getValue())
-                    .forEach(System.out::println);
+
+
+        System.out.println("# elements: " + theResultMapOfUsers.size());
+        theResultMapOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
