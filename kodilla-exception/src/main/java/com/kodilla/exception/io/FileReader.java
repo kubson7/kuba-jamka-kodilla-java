65a7d5e36b9a6f3b42adb605b04b2feecb6f7bcd
kubson10 <kubson9404@gmail.com>
2019-07-08 23:21:43 +0200

    Modul8

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,12 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+
+public class FileReader {
+    public void readFile(){
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/name.txt").getFile());
+
+    }
+}
